313805b87c93d6cdcf80bf4b97487acc867f4b3b
App Academy Student <student@appacademy.io>
2015-12-10 14:11:24 -0800

    Finish phase 3 basic router

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,7 +1,18 @@
 var React = require('react'),
     ReactDOM = require('react-dom'),
+    Router = require('react-router').Router,
+    Route = require('react-router').Route,
+    App = require('./components/app'),
     PokemonsIndex = require('./components/pokemons/pokemons_index');
 
+var router = (
+  <Router>
+    <Route path="/" component={App} />
+  </Router>
+);
+
 document.addEventListener("DOMContentLoaded", function () {
-  ReactDOM.render(<PokemonsIndex/>, document.getElementById("root"));
+  ReactDOM.render(
+    router,
+    document.getElementById("root"));
 });
